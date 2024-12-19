03d656625549af180047c2522d5733fcace77ed4
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-19 18:36:33 +0300

    Fix check PR descr

--- .github/actions/check-pr-desc/index.js
+++ .github/actions/check-pr-desc/index.js
@@ -1,15 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-// const { Octokit } = require("@octokit/core");
+const report = require('../report_utils/report');
 
 
 try {
-
-  // const token = context.token;
-
-  // console.log("Token: " + token);
-  
-  // const octokit = new Octokit({ auth: token });
 
   const pr_body = github.context.payload.pull_request.body;
   const pr_desc = pr_body.substring(
@@ -21,15 +15,10 @@ try {
     core.setOutput("pr_desc");
     core.info("PR description is filled.");
   } else {
-    // octokit.request(
-    //   'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
-    //   {
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    //     issue_number: context.issue.number,
-    //     body: "PR description is not filled!"
-    //   }
-    // );
+    const context = github.context;
+    context.token = core.getInput('github_token');
+
+    report.send(context, "PR assignee is empty.");
     core.setFailed("PR description is not filled!")
   }
 } catch (error) {
