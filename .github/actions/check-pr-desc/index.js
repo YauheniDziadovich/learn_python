3202f0401b34fda29ecb9746f1183dcf1be0af6e
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-19 14:15:14 +0300

    Fix checking PR description

--- .github/actions/check-pr-desc/index.js
+++ .github/actions/check-pr-desc/index.js
@@ -2,12 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  console.log(`Hello worls!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  const pr_body = github.context.payload.pull_request.body;
+  console.log(`Pr body: ${pr_body}`);
+  
+  const pr_desc = pr_body.substring(
+    pr_body.indexOf("## Describe your changes"),
+    pr_body.indexOf("## Checklist before requesting a review")
+  );
+
+  console.log(`PR descr: ${pr_desc}`);
+
+  if (pr_desc) {
+    core.setOutput("pr_desc");
+    core.info("PR description is filled.");
+  } else {
+    core.setFailed("PR description is not filled!")
+  }
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
