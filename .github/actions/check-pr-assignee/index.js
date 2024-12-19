18929f72a8ed17ce49bcfa9a7ba576235b960d67
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-19 17:31:00 +0300

    Fix check PR descr

--- .github/actions/check-pr-assignee/index.js
+++ .github/actions/check-pr-assignee/index.js
@@ -2,18 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const c = github.context.token
+
+  console.log("TOKEN: " + c);
+
   const pr_props = github.context.payload.pull_request;
   const assignees = pr_props.assignees;
   const assignee = pr_props.assignee;
-  
-  console.log("assignees " + assignees);
-  console.log("assignee " + assignees);
-  console.log("assignee " + assignees);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
 
   if (assignee != null || (assignees != undefined && assignees.lenght > 0)) {
     core.setOutput("pr_desc");
