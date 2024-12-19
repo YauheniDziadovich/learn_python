0cafddc70e0ac057c3cdb6ecdf4971c46d10b99a
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-19 15:11:34 +0300

    Add github output

--- .github/actions/check-pr-desc/index.js
+++ .github/actions/check-pr-desc/index.js
@@ -1,11 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { Octokit } = require("@octokit/core");
+// const { Octokit } = require("@octokit/core");
 
 
 try {
 
   const token = process.env.GITHUB_TOKEN;
+
+  console.log(token);
   const octokit = new Octokit({ auth: token });
 
   const pr_body = github.context.payload.pull_request.body;
@@ -18,15 +20,15 @@ try {
     core.setOutput("pr_desc");
     core.info("PR description is filled.");
   } else {
-    octokit.request(
-      'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
-      {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: context.issue.number,
-        body: "PR description is not filled!"
-      }
-    );
+    // octokit.request(
+    //   'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
+    //   {
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    //     issue_number: context.issue.number,
+    //     body: "PR description is not filled!"
+    //   }
+    // );
     core.setFailed("PR description is not filled!")
   }
 } catch (error) {
