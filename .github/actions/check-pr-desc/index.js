53f4e58ee5b49367311af356c4b39497ff2e600a
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-19 14:50:59 +0300

    Add github output

--- .github/actions/check-pr-desc/index.js
+++ .github/actions/check-pr-desc/index.js
@@ -4,19 +4,21 @@ const github = require('@actions/github');
 try {
 
   const pr_body = github.context.payload.pull_request.body;
-  console.log(`Pr body: ${pr_body}`);
-  
   const pr_desc = pr_body.substring(
     pr_body.indexOf("## Describe your changes") + "## Describe your changes".length,
     pr_body.indexOf("## Checklist before requesting a review")
   ).trim();
 
-  console.log(`PR descr: ${pr_desc}, length: ${pr_desc.length}`);
-
   if (pr_desc) {
     core.setOutput("pr_desc");
     core.info("PR description is filled.");
   } else {
+    github.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.issue.number,
+      body: "PR description is not filled!"
+  });
     core.setFailed("PR description is not filled!")
   }
 } catch (error) {
