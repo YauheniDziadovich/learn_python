fcacfc560311fd6a0505c167130347476344ee0c
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-19 17:51:58 +0300

    Fix check PR descr

--- .github/actions/check-pr-assignee/index.js
+++ .github/actions/check-pr-assignee/index.js
@@ -1,11 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const { Octokit } = require("@octokit/rest");
+
 try {
-  const c = github.context.token
-
-  console.log("TOKEN: " + c);
-
   const pr_props = github.context.payload.pull_request;
   const assignees = pr_props.assignees;
   const assignee = pr_props.assignee;
@@ -14,6 +12,18 @@ try {
     core.setOutput("pr_desc");
     core.info("PR assignee is not empty.");
   } else {
+
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN
+      });
+    
+    const context = github.context;
+    const response = await octokit.rest.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+        body: "test message",
+    });
     core.setFailed("PR assignee is empty.")
   }
 
