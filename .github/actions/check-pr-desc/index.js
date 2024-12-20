0ff054409adbc2111b25fdf6517e5ffb66942a2a
Yauheni Dziadovich <y.dziadovich@setplex.com>
2024-12-20 13:41:44 +0300

    Check pr title

--- .github/actions/check-pr-desc/index.js
+++ .github/actions/check-pr-desc/index.js
@@ -5,7 +5,24 @@ const report = require('../report_utils/report');
 
 try {
 
-  const pr_body = github.context.payload.pull_request.body;
+  const context = github.context;
+  const github_token = process.env.GITHUB_TOKEN;
+  context.token = github_token;
+
+  const pull_request = github.context.payload.pull_request;
+
+  // check pull request title mathes commit message pattern
+  const pr_title = pull_request.title.trim();
+  console.log("PR title");
+  const task_pattern='^((MW)|(KIT)|(STR)|(IN)|(MT)|(DEVOPS)|(CR)|(NF)|(IGP))-[0-9]+\. .+$'
+  const release_pattern='^(v[0-9]+\.[0-9]+\.[0-9]+(-rc)?)\. .+$'
+  const hotfix_pattern='^(v[0-9]+\.[0-9]+\.[0-9]+) ((MW)|(KIT)|(STR)|(IN)|(MT))-[0-9]+ (Hotfix\. ).+$'
+  const merge_pattern='^(Merge ).+$'
+
+
+
+  // check pull request description is not empty
+  const pr_body = pull_request.body;
   const pr_desc = pr_body.substring(
     pr_body.indexOf("## Describe your changes") + "## Describe your changes".length,
     pr_body.indexOf("## Checklist before requesting a review")
@@ -15,12 +32,8 @@ try {
     core.setOutput("pr_desc");
     core.info("PR description is filled.");
   } else {
-    const context = github.context;
-    const github_token = process.env.GITHUB_TOKEN;
-    context.token = github_token
-
     report.send(context, "PR description is not filled!");
-    core.setFailed("PR description is not filled!")
+    core.setFailed("PR description is not filled!");
   }
 } catch (error) {
   core.setFailed(error.message);
